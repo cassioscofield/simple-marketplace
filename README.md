ab009bde8e74fdb9dab1d86e1a20096a7ab97ce5
Cassio Scofield <cassio.scofield@ibm.com>
2020-09-29 23:12:05 -0300

    First commit - setup and store model

---
+++ README.md
@@ -0,0 +1,12 @@
+### simple-marketplace
+
+### setup
+```
+npm i
+npm start
+```
+
+### database
+```
+used in memory database persisted in database.json file
+```
---
+++ common/models/store.js
@@ -0,0 +1,50 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-workspace
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+module.exports = function(Store) {
+
+  // Disabling unused remote methods not used
+  Store.disableRemoteMethodByName('upsert');
+  Store.disableRemoteMethodByName('prototype.updateAttributes');
+  Store.disableRemoteMethodByName('exists');
+  Store.disableRemoteMethodByName('createChangeStream');
+  Store.disableRemoteMethodByName('findOne');
+  Store.disableRemoteMethodByName('replaceOrCreate');
+  Store.disableRemoteMethodByName('updateAll');
+  Store.disableRemoteMethodByName('replaceById');
+  Store.disableRemoteMethodByName('replaceOrCreate');
+  Store.disableRemoteMethodByName('upsertWithWhere');
+
+  // Model validations
+  Store.validatesUniquenessOf('name', {
+    message: 'is not unique'
+  });
+  Store.validatesInclusionOf('status', {
+    in: ['active', 'inactive'],
+    message: 'is not allowed'
+  });
+  
+  // Implementing soft-delete
+  Store.on('attached', function () {
+    Store.deleteById = function (id, x, callback) {
+      Store.updateAll({ id: id }, {
+        status: 'inactive',
+      }, callback);
+    };
+  });
+
+  // Filtering only active stores by default
+  Store.beforeRemote('find', function(ctx, modelInstance, next) {
+    if (!ctx.args.filter) {
+      ctx.args.filter = {
+        where: {
+          status: 'active'
+        }
+      };
+    }
+    next();
+  });
+
+};
