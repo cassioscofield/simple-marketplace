d97d341009be0ef4a5f1a903aded18f67f70b7eb
Cassio Scofield <cassio.scofield@ibm.com>
2020-10-01 23:19:31 -0300

    Existing after table creation

--- server/create-tables.js
+++ server/create-tables.js
@@ -14,4 +14,5 @@ var lbTables = [
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
+  ds.disconnect();
 });
